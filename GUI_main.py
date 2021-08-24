d76715ed332a7ecc1c0d877e7e7fc3fa1dde84be
florian <florian.krgl@gmail.com>
2021-08-24 11:33:43 +0200

    add functions to load data and set up the table

--- GUI_main.py
+++ GUI_main.py
@@ -11,14 +11,17 @@ class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
         uic.loadUi('Qt_designer/test_0.ui', self)
-        self.serach_button = self.findChild(QToolButton, 'search_button')
-        self.filter_window = self.findChild(QFrame, 'filter_window')
-        self.b2 = self.findChild(QToolButton, 'button_close')
-        self.table = self.findChild(QTableWidget, 'tableWidget')
-        self.grid_filter_window = self.findChild(QGridLayout, 'grid_filter_window')
+        # menu bar
+
+        # filter
         self.filter_window.hide()
-        self.serach_button.clicked.connect(self.button_action)
-        self.b2.clicked.connect(self.button_close_action)
+
+        # table
+
+        self.actionLoad_File.triggered.connect(self.load_file_from_filebrowser)
+        self.search_button.clicked.connect(self.button_action)
+        self.button_close.clicked.connect(self.button_close_action)
+
         self.show()
 
     def button_action(self):
@@ -27,8 +30,32 @@ class UI(QMainWindow):
     def button_close_action(self):
         self.filter_window.hide()
 
+    def load_file_from_filebrowser(self):
+        filename = QFileDialog.getOpenFileName()
+        file_path = filename[0]
+        if 'vcf' in file_path:
+            self.fill_table(file_path)
+        else:
+            print("The selected file is not in variant call format!")
+            # open Error dialog window
+
+    def fill_table(self, file_path):
+        with open(file_path, 'r') as file:
+            for line in file:
+                if not line.startswith('##'):
+                    if line.startswith('#'):
+                        table_header = line[1:-1].split('\t')
+                        self.vcf_table.setColumnCount(len(table_header))
+                        self.vcf_table.setHorizontalHeaderLabels(table_header)
+                    else:
+                        table_items = line.strip().split('\t')
+                        rowPosition = self.vcf_table.rowCount()
+                        self.vcf_table.insertRow(rowPosition)
+                        for n_col in range(len(table_header)):
+                            self.vcf_table.setItem(rowPosition, n_col, QTableWidgetItem(table_items[n_col]))
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     UIWindow = UI()
     sys.exit(app.exec_())
-333333333333333333333333333333333333333333
